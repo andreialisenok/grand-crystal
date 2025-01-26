4b0efd4d6fdc6f3bf4392555261d45f64ba790df
Andrei Alisenok <andreialisenok@gmail.com>
2025-01-27 00:40:11 +0300

    fix

--- script.js
+++ script.js
@@ -1,19 +1,62 @@
-const swiper = new Swiper('.swiper', {
-  loop: true,
+document.addEventListener('DOMContentLoaded', () => {
+  const burger = document.querySelector('.burger');
+  const mobileMenu = document.querySelector('.mobile-menu');
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+    if (window.innerWidth <= 1199) {
+      document.querySelector('body').classList.toggle('is-lock');
+    }
+  });
+  const swiperPromo = new Swiper('.swiper-promo', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+  new Swiper('.swiper-news', {
+    loop: true,
+    spaceBetween: '16px',
+    slidesPerView: 1.1,
+    breakpoints: {
+      992: {
+        spaceBetween: '40px',
+        slidesPerView: 3,
+      },
+    },
+  });
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+  if (window.innerWidth <= 1599) {
+    const swiperAdvantages = new Swiper('.swiper-advantages', {
+      loop: true,
+      spaceBetween: '16px',
+      slidesPerView: 1.5,
+      breakpoints: {
+        1200: {
+          slidesPerView: 2,
+        },
+      },
+    });
+  }
+  ymaps.ready(init);
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [53.937456, 27.471128],
+        zoom: 14,
+        controls: [],
+      }),
+      myGeoObject = new ymaps.GeoObject({});
+
+    myMap.geoObjects.add(myGeoObject).add(
+      new ymaps.Placemark([53.937456, 27.471128], {
+        iconColor: '#184eca',
+      })
+    );
+  }
 });
